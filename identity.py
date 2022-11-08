1873e7e8031c8afd792d9f317be5131d10f7d943
Marek Nagy <62247071@cuni.cz>
2022-11-08 16:18:17 +0100

    Initial commit

---
+++ identity.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+def main() -> None:
+    # TODO: create DataFrame
+
+    # TODO: create model
+
+    # TODO: choose order of training patterns
+
+    # TODO: train model
+
+    # TODO: discuss results
+    pass
+
+if __name__ == '__main__':
+    main()
